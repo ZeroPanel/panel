44eb36ed3ee9edd3bcec6ede0fa86b4a0321b6de
Nishant Sharma <nishantapps55@gmail.com>
2025-12-25 16:43:03 +0000

    add spacing to admin panel top and make corners bit rounded

--- src/app/(panel)/layout.tsx
+++ src/app/(panel)/layout.tsx
@@ -4,10 +4,10 @@ import { Header } from "@/components/layout/header";
 
 export default function PanelLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen w-full bg-background-dark">
+    <div className="flex h-screen w-full bg-background-dark p-4">
       <SidebarProvider>
           <AppSidebar />
-          <div className="flex flex-1 flex-col h-full overflow-hidden relative">
+          <div className="flex flex-1 flex-col h-full overflow-hidden relative rounded-lg">
               <Header />
               <main className="flex-1 overflow-y-auto p-4 md:p-8">
                   {children}
