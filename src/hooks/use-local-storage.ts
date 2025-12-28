ff7c6cd40dc165ed50576c035ee35452828077d7
Nishant Sharma <nishantapps55@gmail.com>
2025-12-28 06:48:44 +0000

    Try fixing this error: `Console Error: Maximum update depth exceeded. Th

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -2,9 +2,13 @@
 
 import { useState, useEffect, useCallback } from 'react';
 
+// A helper function to determine if we are on the server
+const isServer = typeof window === 'undefined';
+
 export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
+  // Function to read value from localStorage
   const readValue = useCallback((): T => {
-    if (typeof window === 'undefined') {
+    if (isServer) {
       return initialValue;
     }
 
@@ -17,28 +21,39 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T 
     }
   }, [initialValue, key]);
 
-  const [storedValue, setStoredValue] = useState<T>(initialValue);
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(readValue);
 
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
   const setValue = useCallback((value: T | ((val: T) => T)) => {
-    if (typeof window == 'undefined') {
+    if (isServer) {
       console.warn(
         `Tried setting localStorage key “${key}” even though environment is not a client`
       );
     }
 
     try {
-      const newValue = value instanceof Function ? value(storedValue) : value;
-      window.localStorage.setItem(key, JSON.stringify(newValue));
-      setStoredValue(newValue);
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      // We dispatch a custom event so every useLocalStorage hook are notified
       window.dispatchEvent(new Event("local-storage"));
     } catch (error) {
       console.warn(`Error setting localStorage key “${key}”:`, error);
     }
   }, [key, storedValue]);
 
+
   useEffect(() => {
     setStoredValue(readValue());
-  }, [readValue]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [key, readValue]);
 
   useEffect(() => {
     const handleStorageChange = (e: StorageEvent | CustomEvent) => {
@@ -48,7 +63,9 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T 
         setStoredValue(readValue());
     };
 
+    // this only works for other documents, not the current one
     window.addEventListener("storage", handleStorageChange);
+    // this is for the current document
     window.addEventListener("local-storage", handleStorageChange);
 
     return () => {
